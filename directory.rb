f07848383bc3cbdcffc361d315efb141dbd256be
Emma Albury <emma_albury@hotmail.com>
2018-08-28 23:28:46 +0100

    Add name length restriction

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def print_header
 end
 def print(students)
   students.each_with_index do |student, index|
-    if student[:name].start_with?("M")
+    if student[:name].start_with?("M") && student[:name].length < 12
       puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
